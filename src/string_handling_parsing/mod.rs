a239148643bccb45811ad77eb13cab3e9662c22e
benjacoblee <benjacoblee@gmail.com>
2026-06-05 07:19:37 +0800

    more string stuff

--- src/string_handling_parsing/mod.rs
+++ src/string_handling_parsing/mod.rs
@@ -36,3 +36,75 @@ pub fn extract_hash_tags(s: &str) -> Vec<&str> {
         })
         .collect()
 }
+
+pub fn parse_k_v(s: &str) -> HashMap<String, String> {
+    let parts = s.split(',');
+
+    let filtered: Vec<(&str, &str)> = parts
+        .filter_map(|word| match word.split_once("=") {
+            Some((a, b)) => Some((a, b)),
+            _ => None,
+        })
+        .collect();
+
+    filtered.iter().fold(HashMap::new(), |mut acc, (k, v)| {
+        acc.insert(k.to_string(), v.to_string());
+        acc
+    })
+}
+
+pub fn alphanumeric_only(s: &str) -> String {
+    s.chars()
+        .into_iter()
+        .filter_map(|ch| if ch.is_alphanumeric() { Some(ch) } else { None })
+        .collect::<String>()
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Date {
+    pub year: u32,
+    pub month: u32,
+    pub day: u32,
+}
+
+fn is_yearish(y: &str) -> bool {
+    y.len() == 4
+}
+
+fn is_month_dayish(m: &str, d: &str) -> bool {
+    m.len() == 2 && d.len() == 2
+}
+
+pub fn parse_date(s: &str) -> Result<Date, &'static str> {
+    let parts: Vec<&str> = s.split('-').collect();
+    let err: &'static str = "Error parsing date";
+
+    match parts[..] {
+        [y, m, d] => match is_yearish(y) && is_month_dayish(m, d) {
+            false => Err(err),
+            true => {
+                let maybe_y = y.parse::<u32>();
+                let maybe_m = m.parse::<u32>();
+                let maybe_d = d.parse::<u32>();
+
+                match (maybe_y, maybe_m, maybe_d) {
+                    (Ok(year), Ok(month), Ok(day)) => Ok(Date { year, month, day }),
+                    _ => Err(err),
+                }
+            }
+        },
+        _ => Err(err),
+    }
+}
+
+pub fn reverse_words(s: &str) -> String {
+    let strs = s.split(' ').rev().collect::<Vec<&str>>();
+    strs.join(" ")
+}
+
+pub fn normalize_whitespace(s: &str) -> String {
+    s.trim()
+        .split_ascii_whitespace()
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
--- src/string_handling_parsing_tests/mod.rs
+++ src/string_handling_parsing_tests/mod.rs
@@ -38,3 +38,48 @@ fn it_extracts_hashtags() {
     let result = extract_hash_tags(&ms);
     assert_eq!(result, vec!["#wow", "#banana"]);
 }
+
+#[test]
+fn it_parses_key_values() {
+    let my_map: HashMap<String, String> = vec![
+        ("wow".to_string(), "banana".to_string()),
+        ("other".to_string(), "thing".to_string()),
+    ]
+    .into_iter()
+    .collect();
+    let ms = "wow=banana,other=thing";
+    assert_eq!(parse_k_v(&ms), my_map);
+}
+
+#[test]
+fn it_gets_alphanumeric_only() {
+    let res = alphanumeric_only("abc! def;");
+    assert_eq!(res, "abcdef");
+}
+
+#[test]
+fn it_parses_date_correctly() {
+    let r = parse_date("2022-02-02");
+    assert_eq!(
+        r,
+        Ok(Date {
+            year: 2022,
+            month: 2,
+            day: 2
+        })
+    );
+
+    assert!(parse_date("rubbish").is_err());
+}
+
+#[test]
+fn it_reverses_words() {
+    let r = reverse_words("one fine day");
+    assert_eq!(r, "day fine one");
+}
+
+#[test]
+fn it_trims_and_collapses_whitespace() {
+    let r = normalize_whitespace("    a        person    ");
+    assert_eq!(r, "a person")
+}
