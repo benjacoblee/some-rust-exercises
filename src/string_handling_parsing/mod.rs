f42a819a7f2d2ffd56517ed15dfc35dd4b70309e
benjacoblee <benjacoblee@gmail.com>
2026-06-05 04:00:12 +0800

    some string stuff

--- src/string_handling_parsing/mod.rs
+++ src/string_handling_parsing/mod.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use std::num::ParseIntError;
+use std::{collections::HashMap, num::ParseIntError};
 
 pub fn parse_csv_ints(s: &str) -> Result<Vec<i32>, ParseIntError> {
     s.split(',').map(|e| e.parse::<i32>()).collect()
@@ -8,4 +8,31 @@ pub fn parse_csv_ints(s: &str) -> Result<Vec<i32>, ParseIntError> {
 
 pub fn join_comma(v: &[String]) -> String {
     v.join(",")
+}
+
+pub fn first_word(s: &str) -> &str {
+    let words: Vec<&str> = s.split(' ').collect();
+
+    match words.first() {
+        Some(&s) => s,
+        None => "",
+    }
+}
+
+pub fn word_counts(s: &str) -> HashMap<String, usize> {
+    s.split(' ').fold(HashMap::new(), |mut acc, curr| {
+        let lc = curr.to_lowercase();
+        acc.entry(lc).and_modify(|item| *item += 1).or_insert(1);
+
+        acc
+    })
+}
+
+pub fn extract_hash_tags(s: &str) -> Vec<&str> {
+    s.split(' ')
+        .filter_map(|el| match el.starts_with("#") {
+            true => Some(el),
+            false => None,
+        })
+        .collect()
 }
--- src/string_handling_parsing_tests/mod.rs
+++ src/string_handling_parsing_tests/mod.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::collections::HashMap;
+
 use crate::string_handling_parsing::*;
 
 #[cfg(test)]
@@ -13,4 +15,26 @@ fn it_parses_csv_strings_to_int() {
 fn it_joins_commas() {
     let r = join_comma(&vec!["abc".to_string(), "def".to_string()]);
     assert_eq!(r, "abc,def".to_string());
+}
+
+#[test]
+fn it_gets_first_word() {
+    assert_eq!(first_word("mary had"), "mary");
+    assert_eq!(first_word(""), "");
+}
+
+#[test]
+fn it_counts_words() {
+    let my_map: HashMap<String, usize> = vec![("wow".to_string(), 3), ("banana".to_string(), 1)]
+        .into_iter()
+        .collect();
+    let result = word_counts("wow WOW Wow banana");
+    assert_eq!(my_map, result);
+}
+
+#[test]
+fn it_extracts_hashtags() {
+    let ms = "this that #wow #banana something";
+    let result = extract_hash_tags(&ms);
+    assert_eq!(result, vec!["#wow", "#banana"]);
 }
